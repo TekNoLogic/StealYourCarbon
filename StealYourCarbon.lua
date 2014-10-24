c76b989a65b59115ca621b3ac99e4fc2984ba216
tekkub :bear:  <tekkub@gmail.com>
2014-10-24 03:00:26 -0600

    Move water upgrader to its own file

--- StealYourCarbon.lua
+++ StealYourCarbon.lua
@@ -43,42 +43,6 @@ function ns.HasTradeskillBag()
 end
 
 
-local upgrades = {
-	water = {81923,74636,81924,58257,58256,33445,33444,27860,28399,8766,1645,1708,1205,1179,159},
-	intbrew = {89594,89593,89592,89591,89590,89589,89588},
-	agibrew = {89601,89600,89599,89598,89597,89596,89595},
-}
-for _,set in pairs(upgrades) do
-	-- Query server to ensure GetItemInfo doesn't nil out.
-	for _,id in pairs(set) do
-		if not GetItemInfo(id) then GameTooltip:SetHyperlink("item:"..id) end
-	end
-end
-function StealYourCarbon:UpgradeWater()
-	local level = UnitLevel("player")
-	local stocklist = ns.HasTradeskillBag() and self.db.tradestocklist or self.db.stocklist
-
-	for _,set in pairs(upgrades) do
-		local buy, found, oldid = 0
-		for _,id in pairs(set) do
-			if found then
-				buy = buy + (stocklist[id] or 0)
-				if stocklist[id] then oldid = id end
-				stocklist[id] = nil
-			else
-				local _, _, _, _, reqlvl = GetItemInfo(id)
-				if reqlvl and level >= reqlvl then found = id end
-			end
-		end
-
-		if found and buy > 0 then
-			stocklist[found] = buy
-			ns.PrintF("Upgrading %s to %s", select(2, GetItemInfo(oldid)), select(2, GetItemInfo(found)))
-		end
-	end
-end
-
-
 ----------------------
 --      Events      --
 ----------------------
@@ -99,7 +63,7 @@ end
 local _, _, _, _, _, TRADE_GOODS = GetAuctionItemClasses()
 ns.RegisterEvent("MERCHANT_SHOW", function()
 	local hastradebag = ns.HasTradeskillBag()
-	StealYourCarbon:UpgradeWater()
+	ns.UpgradeWater()
 	local spent, stocklist = 0, hastradebag and StealYourCarbon.db.tradestocklist or StealYourCarbon.db.stocklist
 	for i=1,GetMerchantNumItems() do
 		local link = GetMerchantItemLink(i)
